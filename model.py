79ecd72ab191c4dd3c7ae8f17dce61794f49092c
muskanvaswan <muskanvaswan@gmail.com>
2020-12-22 22:40:53 +0530

    addign compare and distance feature for all faces in the directory with a single picture

--- model.py
+++ model.py
@@ -1,10 +1,22 @@
 import face_recognition as fr
+import numpy as np
 import cv2
+import os
 
-img = fr.load_image_file('images/shreya.jpeg')
-locations = fr.face_locations(img)
-locations = locations[0]
+def encode_image(path):
+    img = fr.load_image_file(f"images/{path}")
+    return fr.face_encodings(img)[0]
 
-img = cv2.rectangle(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), (locations[1], locations[0]), (locations[3], locations[2]), (0,255,0), 2)
-cv2.imshow("shreya", img)
-cv2.waitKey(0)
+encodings = np.array(list(map(encode_image, os.listdir('images/'))))
+image = fr.load_image_file("images/muskan.png")
+image = fr.face_encodings(image)[0]
+print(fr.compare_faces(encodings, image))
+print(fr.face_distance(encodings, image))
+
+
+# img = fr.load_image_file('images/shreya.jpeg')
+# locations = fr.face_locations(img)
+# locations = locations[0]
+#
+# img = cv2.rectangle(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), (locations[1], locations[0]), (locations[3], locations[2]), (0,255,0), 2)
+# cv2.imshow("shreya", img)
