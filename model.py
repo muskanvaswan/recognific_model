f12b79813f9614eacc4c9219dc2d2645afd27aa7
muskanvaswan <muskanvaswan@gmail.com>
2020-12-24 03:11:06 +0530

    removing encode from main model

--- model.py
+++ model.py
@@ -3,13 +3,11 @@ import numpy as np
 import cv2
 import os
 
+from reader import reading_encodings
+
 IMAGES_LIST = os.listdir('images/')
-
-def encode_image(path):
-    img = fr.load_image_file(f"images/{path}")
-    return fr.face_encodings(img)[0]
-
-encodings = np.array(list(map(encode_image, IMAGES_LIST)))
+encodings = reading_encodings()['encodings']
+#encodings = np.array(list(map(encode_image, IMAGES_LIST)))
 # image = fr.load_image_file("images/muskan.png")
 # image = fr.face_encodings(image)[0]
 # print(fr.compare_faces(encodings, image))
@@ -38,7 +36,7 @@ for i in range(10):
     frame = cv2.putText(frame, name,  (locations[3], locations[2]+20), cv2.FONT_HERSHEY_SIMPLEX ,  1, (0,255,0), 2, cv2.LINE_AA)
 
     cv2.imshow("webcam", frame)
-    cv2.waitKey(20)
+    cv2.waitKey(2000)
     # if cv2.waitKey(1) & 0xFF == ord('q'):
         # break
 # img = fr.load_image_file('images/shreya.jpeg')
