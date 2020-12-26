827cc54492c81c6d4fe273d113b82e2b6d4f8901
arushisingla <arushisingla1911@gmail.com>
2020-12-26 19:40:14 +0530

    Converting csv attendance sheet to excel format

--- reader.py
+++ reader.py
@@ -1,5 +1,6 @@
 import pickle
 import csv
+import pandas as pd
 
 def reading_encodings(path):
     # reading the data from the file
@@ -13,3 +14,11 @@ def csv_writter(ls):
     with open('Attendance/sample.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(ls)
+        excel_writer()
+
+def excel_writer():
+    # Reading the csv file
+    read_file = pd.read_csv('Attendance/sample.csv')
+
+    # saving xlsx file
+    read_file.to_excel('Attendance/sample.xlsx', index = None,header=True)
