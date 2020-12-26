ca1c311d965ef46568674b2e1fd89b56b913d53d
muskanvaswan <muskanvaswan@gmail.com>
2020-12-26 15:48:59 +0530

    added most likely face recognition as final result

--- model.py
+++ model.py
@@ -17,28 +17,37 @@ webcam = cv2.VideoCapture(0)
 if not webcam.read(0)[0]:
     webcam = cv2.VideoCapture(1)
 
-for i in range(10):
-    status, frame = webcam.read()
+recognised = []
 
-    try:
-        locations = fr.face_locations(frame)
-        locations = locations[0]
+while(recognised==[]):
+    for i in range(10):
+        status, frame = webcam.read()
 
-        image = fr.face_encodings(frame)[0]
-        matches = fr.face_distance(encodings, image)
-        i = np.where(matches==min(matches))[0][0]
-        name = IMAGES_LIST[i].split('.')[0]
-    except:
-        locations = (0,0,0,0)
-        name =''
+        try:
+            locations = fr.face_locations(frame)
+            locations = locations[0]
 
-    frame = cv2.rectangle(frame, (locations[1], locations[0]), (locations[3], locations[2]), (0,255,0), 2)
-    frame = cv2.putText(frame, name,  (locations[3], locations[2]+20), cv2.FONT_HERSHEY_SIMPLEX ,  1, (0,255,0), 2, cv2.LINE_AA)
+            image = fr.face_encodings(frame)[0]
+            matches = fr.face_distance(encodings, image)
+            i = np.where(matches==min(matches))[0][0]
+            name = IMAGES_LIST[i].split('.')[0]
+            recognised.append(name)
 
-    cv2.imshow("webcam", frame)
-    cv2.waitKey(2000)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-        # break
+        except:
+            locations = (0,0,0,0)
+            name =''
+
+        frame = cv2.rectangle(frame, (locations[1], locations[0]), (locations[3], locations[2]), (0,255,0), 2)
+        frame = cv2.putText(frame, name,  (locations[3], locations[2]+20), cv2.FONT_HERSHEY_SIMPLEX ,  1, (0,255,0), 2, cv2.LINE_AA)
+
+        cv2.imshow("webcam", frame)
+        cv2.waitKey(20)
+
+print(recognised)
+person = max(recognised, key = recognised.count)
+print(person)
+
+
 # img = fr.load_image_file('images/shreya.jpeg')
 # locations = fr.face_locations(img)
 # locations = locations[0]
